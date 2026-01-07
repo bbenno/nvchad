40a9feb782744c841fd4aaaf7e8e5e5d162c9712
Benno Bielmeier <git@bbenno.com>
2026-01-07 14:54:48 +0100

    tmp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
-vim.lsp.config("nixd", {
+local lspconfig = require "lspconfig"
+
+lspconfig.html.setup({})
+lspconfig.cssls.setup({})
+
+lspconfig.nixd.setup({
   settings = {
     nixd = {
       nixpkgs = {
@@ -11,21 +16,42 @@ vim.lsp.config("nixd", {
       },
       options = {
         nixos = {
-          expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations."<hostname>".options',
+          expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations."' .. vim.fn.hostname() .. '".options',
         },
-        home_manager = {
-          expr = '(builtins.getFlake "/etc/nixos").homeConfigurations."<user>".options',
-        },
+        -- home_manager = {
+        --   expr = '(builtins.getFlake "/etc/nixos").homeConfigurations."<user>".options',
+        -- },
       },
     },
   },
 })
+
+-- vim.lsp.config("nixd", {
+--   settings = {
+--     nixd = {
+--       nixpkgs = {
+--         expr = "import <nixpkgs> {}",
+--       },
+--       formatting = {
+--         command = { "nixfmt" }, -- optional; formatting still via conform
+--       },
+--       options = {
+--         nixos = {
+--           expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations."<hostname>".options',
+--         },
+--         home_manager = {
+--           expr = '(builtins.getFlake "/etc/nixos").homeConfigurations."<user>".options',
+--         },
+--       },
+--     },
+--   },
+-- })
 
 local servers = {
   "html",
   "cssls",
   "nixd",
 }
-vim.lsp.enable(servers)
+-- vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
