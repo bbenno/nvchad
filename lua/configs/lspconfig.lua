eed6cb5d6ce46bc6983c5ed47b1fe8b92d1527cb
Benno Bielmeier <git@bbenno.com>
2026-01-07 14:40:21 +0100

    add nix lsp and formater

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,31 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.config("nixd", {
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> {}",
+      },
+      formatting = {
+        command = { "nixfmt" }, -- optional; formatting still via conform
+      },
+      options = {
+        nixos = {
+          expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations."<hostname>".options',
+        },
+        home_manager = {
+          expr = '(builtins.getFlake "/etc/nixos").homeConfigurations."<user>".options',
+        },
+      },
+    },
+  },
+})
+
+local servers = {
+  "html",
+  "cssls",
+  "nixd",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
